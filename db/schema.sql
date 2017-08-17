34cd2ebdac1e095f4fecf0c122d37b944ad47425
Alec Schleicher <schleicher@3cisd.com>
2017-08-16 20:49:19 -0400

    Added db setup

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(100) NOT NULL,
+devoured BOOL NOT NULL,
+date TEIMSTAMP NOT NULL,
+PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES
+('Cheeseburger', false, '2017-08-16 20:41:23.000000'),
+('Double Bacon Whopper', false, '2017-08-16 20:41:23.000000'),
+('The Heart Attacker', false, '2017-08-16 20:41:23.000000');

92c3cb5d046b547a3d2c0a68ed0a4de7242e2d9d
Alec Schleicher <schleicher@3cisd.com>
2017-08-23 21:36:11 -0400

    minor fix

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,11 @@
 if (process.env.JAWSDB_URL) {
   const connection = require('mysql').createConnection(process.env.JAWSDB_URL);
+   connection.connect((err) => {
+    if (err) throw err;
+      console.log("Connected as ID: " + connection.threadId);
+    });
+
+    module.exports = connection;
 } else {
   const connection = require('mysql').createConnection({
     host: "localhost",
@@ -8,12 +14,12 @@ if (process.env.JAWSDB_URL) {
     password: "Groundon121!",
     database: "burgers_db"
   });
+
+  connection.connect((err) => {
+  if (err) throw err;
+    console.log("Connected as ID: " + connection.threadId);
+  });
+
+  module.exports = connection;
 }
 
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected as ID: " + connection.threadId);
-});
-
-module.exports = connection;
